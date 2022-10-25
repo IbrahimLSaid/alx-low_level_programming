a192a79f30dd914ed9271d69ab9ebaf3f646a7bc
IbrahimLSaid <73036859+IbrahimLSaid@users.noreply.github.com>
2022-10-25 04:26:01 +0200

    puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,36 +1,18 @@
 #include "holberton.h"
 
-
-
 /**
-
- * puts2 - prints every other character of a  string to stdout
-
- * @str: string to print
-
- *
-
- * Return: void
-
- */
-
+* puts2 - prints every other character of a  string to stdout
+* @str: string to print
+*
+* Return: void
+*/
 void puts2(char *str)
-  
 {
-  
-  int i = 0;
-  
-  while (*(str + i))
-    
-    {
-      
-      _putchar(*(str + i));
-      
-      i = i + 2;
-      
-    }
-  
-  _putchar('\n');
-  
+int i = 0;
+while (*(str + i))
+{
+_putchar(*(str + i));
+i = i + 2;
 }
-
+_putchar('\n');
+}
