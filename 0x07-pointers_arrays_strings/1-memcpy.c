e38d0fd4473936b293473940d7a3d3cd7e640357
IbrahimLSaid <Ibrahim.l.said@hotmail.com>
2022-10-31 10:05:08 -0700

    retry3

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+void *_memcpy(void *dest, const void *src, size_t n)
+{
+unsigned int index;
+unsigned char *destination = dest;
+const unsigned char *source = src;
+for (index = 0; index < n; index++)
+destination[index] = source[index];
+return (dest);
+}
