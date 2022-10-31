33e6fb6d1290bdb119eb6027b5e6160debb236cb
IbrahimLSaid <Ibrahim.l.said@hotmail.com>
2022-10-31 09:53:28 -0700

    retry2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,7 @@
 #include "holberton.h"
-
 void *_memset(void *s, int c, size_t n)
 {
-  
-unsigned int index;                   
+unsigned int index;
 unsigned char *memory = s, value = c;
 for (index = 0; index < n; index++)
 memory[index] = value;
